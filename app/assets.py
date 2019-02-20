f0119f077cf62efaec737b27f89e23fdcdbfb951
Sarah Raines <saraines@Sarahs-MacBook-Pro.local>
2019-02-19 23:44:38 -0500

    frontend for database search tool

--- app/assets.py
+++ app/assets.py
@@ -4,12 +4,13 @@ app_css = Bundle('app.scss', filters='scss', output='styles/app.css')
 
 app_js = Bundle('app.js', filters='jsmin', output='scripts/app.js')
 
-vendor_css = Bundle('vendor/semantic.min.css', output='styles/vendor.css')
+vendor_css = Bundle('vendor/semantic.min.css', 'vendor/calendar.min.css', output='styles/vendor.css')
 
 vendor_js = Bundle(
     'vendor/jquery.min.js',
     'vendor/semantic.min.js',
     'vendor/tablesort.min.js',
     'vendor/zxcvbn.js',
+    'vendor/calendar.min.js',
     filters='jsmin',
     output='scripts/vendor.js')
