96230eaf7d2b058f37a5bc120c4dc1ac09afbc21
sarahraines <saraines@seas.upenn.edu>
2019-03-03 13:52:39 -0500

    tag and suggestion forms insert data into database

--- app/suggestion/views.py
+++ app/suggestion/views.py
@@ -7,14 +7,26 @@ from sqlalchemy.exc import IntegrityError
 from app import db
 from app.suggestion.forms import SuggestionForm
 from app.decorators import admin_required
+from app.models import Suggestion
 
 suggestion = Blueprint('suggestion', __name__)
 
 
-@suggestion.route('/')
+@suggestion.route('/', methods=['GET', 'POST'])
 def index():
     """Suggestion page."""
     form = SuggestionForm()
-    return render_template(
-        'suggestion/index.html',
-        form=form)
+
+    if form.validate_on_submit():
+        suggestion = Suggestion(
+            title=form.title.data, link=form.link.data,
+            description=form.description.data)
+        db.session.add(suggestion)
+        db.session.commit()
+        flash(
+            'Suggestion \"{}\" successfully created'.format(
+                form.title.data), 'form-success')
+        return render_template(
+            'suggestion/index.html', form=form)
+
+    return render_template('suggestion/index.html', form=form)
--- app/tag/views.py
+++ app/tag/views.py
@@ -12,9 +12,21 @@ from app.models import Tag
 tag = Blueprint('tag', __name__)
 
 
-@tag.route('/')
+@tag.route('/', methods=['GET', 'POST'])
 def index():
     """Tag Management page."""
     form = TagForm()
     tags = Tag.query.all()
+
+    if form.validate_on_submit():
+        tag = Tag(name=form.name.data)
+        db.session.add(tag)
+        db.session.commit()
+        flash(
+            'Tag \"{}\" successfully created'.format(
+                form.name.data), 'form-success')
+        tags = Tag.query.all()
+        return render_template(
+            'tag/index.html', form=form, tags=tags)
+
     return render_template('tag/index.html', form=form, tags=tags)
